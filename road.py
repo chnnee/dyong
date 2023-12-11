0d8cbaf36857b60094985dc9c56ae388a5d5b930
Heewon Kim <heewonk43@naver.com>
2023-12-12 02:50:43 +0900

    새 검출 색상 수정

--- road.py
+++ road.py
@@ -7,6 +7,8 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 cap = cv2.VideoCapture(video_path)
 
+previous_boxes = []
+
 while True:
     ret, frame = cap.read()
 
@@ -14,13 +16,26 @@ while True:
         break
 
     boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8), padding=(4, 4), scale=1.05)
+    new_boxes = []
 
     for (x, y, w, h) in boxes:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        current_box_center = (x + w // 2, y + h // 2)
+        new_box = True
 
+        for (prev_x, prev_y, prev_w, prev_h) in previous_boxes:
+            previous_box_center = (prev_x + prev_w // 2, prev_y + prev_h // 2)
 
-    cv2.putText(frame, f'Pedestrians: {len(boxes)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            if abs(current_box_center[0] - previous_box_center[0]) < 50 and abs(current_box_center[1] - previous_box_center[1]) < 50:
+                new_box = False
+                break
+        if new_box:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            new_boxes.append((x, y, w, h))
+        else:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+    cv2.putText(frame, f'Pedestrians: {len(new_boxes)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    previous_boxes = new_boxes
 
     cv2.imshow('Pedestrian Detection', frame)
 
